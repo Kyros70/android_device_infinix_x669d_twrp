d2c6e8168c3548f8d13137c2a7ef5d0d0a80d57d
Kyros70 <zaedmasri000999@gmail.com>
2026-07-22 02:50:49 +0300

    Add thermal driver

--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -4,12 +4,13 @@ load_panel()
    {
    	insmod /lib/modules/kfifo_buf.ko
    	insmod /lib/modules/sensorhub.ko
-        insmod /lib/modules/tran_gesture.ko
+      insmod /lib/modules/tran_gesture.ko
    	insmod /lib/modules/adaptive-ts.ko
    	insmod /lib/modules/gpio.ko
    	insmod /lib/modules/focaltech_ft8756_spi_ts.ko
-        insmod /lib/modules/sc27xx-vibra.ko
-        insmod /lib/modules/sc27xx_adc.ko
+      insmod /lib/modules/sc27xx-vibra.ko
+      insmod /lib/modules/sc27xx_adc.ko
+      insmod /lib/modules/thermal-generic-adc.ko
    }
 
 load_panel
