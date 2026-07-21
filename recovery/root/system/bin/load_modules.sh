a0e86bc1f5e751613035a766a14fe9d712c5555c
Kyros70 <zaedmasri000999@gmail.com>
2026-07-21 18:55:37 +0300

    Touch fix by @nyaniscool & fix fstab and flags.

--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -1,22 +1,16 @@
 #!/sbin/sh
 
 load_panel()
-{
-	insmod /lib/modules/adaptive-ts.ko
-	insmod /lib/modules/gpio.ko
-	insmod /lib/modules/gpio-sprd.ko
-	insmod /lib/modules/focaltech_ft8756_spi_ts.ko
+   {
+   	insmod /lib/modules/kfifo_buf.ko
+   	insmod /lib/modules/sensorhub.ko
+        insmod /lib/modules/tran_gesture.ko
+   	insmod /lib/modules/adaptive-ts.ko
+   	insmod /lib/modules/gpio.ko
+   	insmod /lib/modules/focaltech_ft8756_spi_ts.ko
         insmod /lib/modules/sc27xx-vibra.ko
-        insmod /lib/modules/sprd-sc27xx-spi.ko
-        insmod /lib/modules/sc27xx-adc.ko
-	insmod /lib/modules/goodix_fp.ko
-	insmod /lib/modules/fpsensor_fp.ko
-	insmod /lib/modules/fortsense_fp.ko
-	insmod /lib/modules/spi-sprd.ko
-	insmod /lib/modules/spi-sprd-adi.ko
-	insmod /lib/modules/spipe.ko
-}
-
+        insmod /lib/modules/sc27xx_adc.ko
+   }
 
 load_panel
 wait 1
