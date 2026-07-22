1d0b8164ae0a85de27709de4b6a8f7a44bb74126
Kyros70 <zaedmasri000999@gmail.com>
2026-07-22 23:12:52 +0300

    Add back load_modules.sh to load touch modules

---
+++ recovery/root/system/bin/load_modules.sh
@@ -0,0 +1,18 @@
+#!/sbin/sh
+
+load_panel()
+   {
+   	insmod /lib/modules/kfifo_buf.ko
+   	insmod /lib/modules/sensorhub.ko
+        insmod /lib/modules/tran_gesture.ko
+   	insmod /lib/modules/adaptive-ts.ko
+   	insmod /lib/modules/gpio.ko
+   	insmod /lib/modules/focaltech_ft8756_spi_ts.ko
+        insmod /lib/modules/sc27xx-vibra.ko
+        insmod /lib/modules/sc27xx_adc.ko
+   }
+
+load_panel
+wait 1
+setprop modules.loaded 1
+exit 0
